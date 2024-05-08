ef098bc277d2ed60abf2b036d166715cdaa8a653
Umamcode <afif.optik04@gmail.com>
2024-05-08 14:53:10 +0700

    Project1-Puskesmas

--- paramedik/sidebar.php
+++ paramedik/sidebar.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="../home.php" class="brand-link">
+    <a href="../index.html" class="brand-link">
         <img src="../dist/img/logoUA.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Puskesmas UA Medika</span>
     </a>
